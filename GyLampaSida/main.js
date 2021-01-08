852f4fd500f01c43dae5ad695d74c24fbede5913
Andi0249 <74541930+Andi0249@users.noreply.github.com>
2021-01-08 23:41:00 +0100

    Add files via upload

---
+++ GyLampaSida/main.js
@@ -0,0 +1,18 @@
+var ProductImg = document.getElementById("ProductImg");
+var lampminis = document.getElementsByClassName("lampmini");
+
+lampminis[0].onclick = function()
+{
+    ProductImg.src = lampminis[0].src;
+}
+
+lampminis[1].onclick = function()
+{
+    ProductImg.src = lampminis[1].src;
+}
+
+lampminis[2].onclick = function()
+{
+    ProductImg.src = lampminis[2].src;
+}
+
